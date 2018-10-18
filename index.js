f4c7e41840265f7a7add65d443691026d2c3ec48
Lu Xie <xiemegan447@gmail.com>
2018-10-17 22:00:59 -0400

    mideterm edit 10.17.2018

--- index.js
+++ index.js
@@ -14,15 +14,19 @@ function popOne () {
   const close = document.createElement('button')
   close.id = 'close'
   close.textContent = 'X'
+  const div = document.createElement('div')
+  div.id = 'around'
   const img = document.createElement('img')
   img.src = './imgs/portfolios/ATBI_discover.png'
   const p = document.createElement('p')
-  p.textContent = 'This is my test'
+  p.textContent = 'This is my first program wrote by AngularJS, by 2018 Summer.The detail is in my Github.'
   popUp.appendChild(close)
-  popUp.appendChild(img)
-  popUp.appendChild(p)
+  popUp.appendChild(div)
+  div.appendChild(img)
+  div.appendChild(p)
   popUp.classList.remove('hidden')
   container.style.opacity = 0.5
+
   close.addEventListener('click', function () {
     popUp.classList.add('hidden')
     container.style.opacity = 1
@@ -33,13 +37,16 @@ function popTwo () {
   const close = document.createElement('button')
   close.id = 'close'
   close.textContent = 'X'
+  const div = document.createElement('div')
+  div.id = 'around'
   const img = document.createElement('img')
-  img.src = './imgs/GitHub.jpg'
+  img.src = './imgs/portfolios/shopify-challenge.png'
   const p = document.createElement('p')
-  p.textContent = 'This is my test'
+  p.textContent = 'This project is I wrote by HTML, CSS and JS as a challenge test. The detail is in my Github.'
   popUp.appendChild(close)
-  popUp.appendChild(img)
-  popUp.appendChild(p)
+  popUp.appendChild(div)
+  div.appendChild(img)
+  div.appendChild(p)
   popUp.classList.remove('hidden')
   container.style.opacity = 0.5
   close.addEventListener('click', function () {
@@ -52,13 +59,16 @@ function popThree () {
   const close = document.createElement('button')
   close.id = 'close'
   close.textContent = 'X'
+  const div = document.createElement('div')
+  div.id = 'around'
   const img = document.createElement('img')
-  img.src = './imgs/GitHub.jpg'
+  img.src = './imgs/portfolios/color-game.png'
   const p = document.createElement('p')
-  p.textContent = 'This is my test'
+  p.textContent = 'This project was my level 2 UX design work, which uses SVG and JavaScript. The detail is in my Github.'
   popUp.appendChild(close)
-  popUp.appendChild(img)
-  popUp.appendChild(p)
+  popUp.appendChild(div)
+  div.appendChild(img)
+  div.appendChild(p)
   popUp.classList.remove('hidden')
   container.style.opacity = 0.5
   close.addEventListener('click', function () {
@@ -71,13 +81,16 @@ function popFour () {
   const close = document.createElement('button')
   close.id = 'close'
   close.textContent = 'X'
+  const div = document.createElement('div')
+  div.id = 'around'
   const img = document.createElement('img')
-  img.src = './imgs/GitHub.jpg'
+  img.src = './imgs/portfolios/mydrawing1.png'
   const p = document.createElement('p')
-  p.textContent = 'This is my test'
+  p.textContent = ''
   popUp.appendChild(close)
-  popUp.appendChild(img)
-  popUp.appendChild(p)
+  popUp.appendChild(div)
+  div.appendChild(img)
+  div.appendChild(p)
   popUp.classList.remove('hidden')
   container.style.opacity = 0.5
   close.addEventListener('click', function () {
@@ -90,13 +103,16 @@ function popFive () {
   const close = document.createElement('button')
   close.id = 'close'
   close.textContent = 'X'
+  const div = document.createElement('div')
+  div.id = 'around'
   const img = document.createElement('img')
-  img.src = './imgs/GitHub.jpg'
+  img.src = './imgs/portfolios/myposter.png'
   const p = document.createElement('p')
-  p.textContent = 'This is my test'
+  p.textContent = ' '
   popUp.appendChild(close)
-  popUp.appendChild(img)
-  popUp.appendChild(p)
+  popUp.appendChild(div)
+  div.appendChild(img)
+  div.appendChild(p)
   popUp.classList.remove('hidden')
   container.style.opacity = 0.5
   close.addEventListener('click', function () {
